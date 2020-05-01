768e3d69dbe7d75277c77be191c5a373b323f1cc
Alejandro Prieto <prieto@cm3p.de>
2020-05-01 16:20:53 +0200

    Initial Commit

---
+++ FirstARTests/FirstARTests.swift
@@ -0,0 +1,34 @@
+//
+//  FirstARTests.swift
+//  FirstARTests
+//
+//  Created by Alejandro Prieto on 01.05.20.
+//  Copyright © 2020 Alejandro Prieto. All rights reserved.
+//
+
+import XCTest
+@testable import FirstAR
+
+class FirstARTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
